2e7b039ad25039e89f42f5c6c4b7515473fb5317
Mario Oklevski <oklevskimarioo@gmail.com>
2025-02-03 03:05:56 +0100

    Logic change

--- today.py
+++ today.py
@@ -449,6 +449,7 @@ if __name__ == '__main__':
     OWNER_ID, acc_date = user_data
     formatter('account data', user_time)
     age_data, age_time = perf_counter(daily_readme, datetime.datetime(1999, 3, 16))
+    print(age_data)
     formatter('age calculation', age_time)
     total_loc, loc_time = perf_counter(loc_query, ['OWNER', 'COLLABORATOR', 'ORGANIZATION_MEMBER'], 7)
     formatter('LOC (cached)', loc_time) if total_loc[-1] else formatter('LOC (no cache)', loc_time)
